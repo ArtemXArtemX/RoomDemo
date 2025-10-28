c620f3f020ea3dcdf1c58a2757154199a318a7a9
tematrkv <tatarnikov.stud@mail.ru>
2025-10-28 21:24:35 +0700

    VM factory + ScreenSetup + observeAsState skeleton

--- app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -1,28 +1,38 @@
 package com.example.roomdemo
 
+import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.roomdemo.ui.theme.RoomDemoTheme
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            RoomDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                val owner = LocalViewModelStoreOwner.current
+                owner?.let {
+                    val viewModel: MainViewModel = viewModel(
+                        it,
+                        "MainViewModel",
+                        MainViewModelFactory(LocalContext.current.applicationContext as Application)
+                    )
+                    ScreenSetup(
+                        modifier = Modifier.padding(innerPadding),
+                        viewModel = viewModel
                     )
                 }
             }
@@ -30,18 +40,32 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+class MainViewModelFactory(private val application: Application) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MainViewModel(application) as T
+    }
+}
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ScreenSetup(modifier: Modifier = Modifier, viewModel: MainViewModel) {
+    val allProducts by viewModel.allProducts.observeAsState(listOf())
+    val searchResults by viewModel.searchResults.observeAsState(listOf())
+    MainScreen(
+        modifier = modifier,
+        allProducts = allProducts,
+        searchResults = searchResults,
+        viewModel = viewModel
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    RoomDemoTheme {
-        Greeting("Android")
-    }
+fun MainScreen(
+    modifier: Modifier = Modifier,
+    allProducts: List<Product>,
+    searchResults: List<Product>,
+    viewModel: MainViewModel
+) {
+
 }
